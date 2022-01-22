b68d7d40c630a9bfddb61d5942497f164af9e0b3
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-22 17:32:51 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,11 +25,13 @@ date_default_timezone_set('Asia/Kolkata');
 |
 */
 if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
-	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/myc";
+	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/mycranium";
 }
 else{
-	$config['base_url'] = "";
+	$config['base_url'] = "https://demo.softwarebss.com/myc/";
 }
+
+
 
 /*
 |--------------------------------------------------------------------------
